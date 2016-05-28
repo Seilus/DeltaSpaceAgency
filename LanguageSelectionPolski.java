aafa163223ded2988d0bc7cd2cfb1033ade01cd3
maniama <hanabipirate@gmail.com>
2016-05-28 15:31:56 +0200

    Update LanguageSelectionPolski.java

--- LanguageSelectionPolski.java
+++ LanguageSelectionPolski.java
@@ -10,22 +10,32 @@ public class LanguageSelectionPolski {
 		public LanguageSelectionPolski(){
 			
 		  menu[0]="Start";
-		  menu[1]="Naciśnij przycisk żeby rozpocząć";
-	      menu[2]="Wybierz misję";
-		  menu[3]= "Wybierz język";
+		  menu[1]="Naci\u015bnij przycisk \u017ceby rozpocz\u0105\u0107";
+	      menu[2]="Wybierz misj\u0119";
+		  menu[3]= "Wybierz j\u0119zyk";
 		  menu[4]="Pomoc";
 			
 		 game[0]="SILNIK";
-		 game[1]="Predkosc statku";
-		 game[2]="Current throttle";
+		 game[1]="Predko\u015bc statku";
+		 game[2]="Aktualna przepustnica";
 		 game[3]="Aktualna masa statku";
-		 game[4]="Pozostale paliwo";
+		 game[4]="Pozosta\u0142e paliwo";
 		 
-		 game[5]="Nazwa najblizszej planety";
-		 game[6]="Odleglosc do najblizszej planety";
-		 game[7]="X odleglosc do planety";
-		 game[8]="Y odleglosc do planety";
+		 game[5]="Nazwa najbli\u017cszej planety";
+		 game[6]="Odleg\u0142o\u015b\u0107 do najbli\u017cszej planety";
+		 game[7]="X odleg\u0142o\u015b\u0107 do planety";
+		 game[8]="Y odleg\u0142o\u015b\u0107 do planety";
 		 
 		
 		}
 }
+/*Ą - \u0104; ą - \u0105
+Ć - \u0106; ć - \u0107
+Ę - \u0118; ę - \u0119
+Ł - \u0141; ł - \u0142
+Ń - \u0143; ń - \u0144
+Ó - \u00d3; ó - \u00f3
+Ś - \u015a; ś - \u015b
+Ź - \u0179; ź - \u017a
+Ż - \u017b; ż - \u017c
+*/
