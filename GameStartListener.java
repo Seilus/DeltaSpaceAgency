4327945a7562bf50fdd4075277901143d2662159
maniama <hanabipirate@gmail.com>
2016-05-28 12:50:36 +0200

    Update GameStartListener.java

--- GameStartListener.java
+++ GameStartListener.java
@@ -6,17 +6,25 @@ import java.awt.event.ActionListener;
 public class GameStartListener implements ActionListener {
 	
 	GameWindow window;
+	MenuFrame menuFrame;
 	GameAnimation animation;
+	
+	void setMenuFrame(MenuFrame menuFrame){
+		this.menuFrame=menuFrame;
+	}
 	
 	public GameStartListener(GameWindow window, GameAnimation animation){
 		this.window=window;
 		this.animation=animation;
+		
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		
 		window.setVisible(true);
 		animation.animationStart();
+		menuFrame.setVisible(false);;
 	}
 
 }
