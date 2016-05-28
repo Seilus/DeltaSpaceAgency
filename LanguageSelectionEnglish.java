4225c48e182063fd8b40d7a50d2644773cf0fefa
maniama <hanabipirate@gmail.com>
2016-05-28 12:48:00 +0200

    Update LanguageSelectionEnglish.java

--- LanguageSelectionEnglish.java
+++ LanguageSelectionEnglish.java
@@ -1,20 +1,29 @@
 package pl.edu.pw.fizyka.pojava.OddzialDelta;
 
 public class LanguageSelectionEnglish {
-	String  menu[]=new String[4];
+	String  menu[]=new String[5];
 	String missions[]=new String[2];
-	
+	String game[]=new String[9];
 		public LanguageSelectionEnglish(){
 		
 		  menu[0]="Start";
-		 menu[1]="Press button to start";
-	     menu[2]="Choose misson";
-		menu[3]	= "Choose language";
+		  menu[1]="Press button to start";
+	      menu[2]="Choose misson";
+		  menu[3]	= "Choose language";
+		  menu[4]="Help";
 			
-		 missions[0]="Mission 1";
-		 missions[1]="Mission 2";
+		  missions[0]="Mission 1";
+		  missions[1]="Mission 2";
 		 
-		
+		 game[0]="Engine";
+		 game[1]="Ship speed";
+		 game[2]="Current throttle";
+		 game[3]="Current ship mass";
+		 game[4]="Remaining Fuel";
+		 
+		 game[5]="Name of the nearest planet";
+		 game[6]="Distance to nearest planet";
+		 game[7]="X distance to planet";
+		 game[8]="Y distance to planet";
 		}
-	
 }
