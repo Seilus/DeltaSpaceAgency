6b8acd4c7b732bd35b9b55e1cc4805564cfe7a3d
maniama <hanabipirate@gmail.com>
2016-05-28 12:47:10 +0200

    Update LanguageChooserListener.java

--- LanguageChooserListener.java
+++ LanguageChooserListener.java
@@ -8,9 +8,9 @@ import javax.swing.JComboBox;
 import javax.swing.JLabel;
 
  public  class LanguageChooserListener implements ActionListener{
-	 public String Langmenu []=new String[4];
+	 public String Langmenu []=new String[5];
 	 public String[] Languages ={"polski","english"};
-	public int num=5;
+	
 	 JComboBox LanguageComboBox=new JComboBox(Languages);
 	 JLabel test,test2;
 	 
@@ -18,48 +18,34 @@ import javax.swing.JLabel;
 			this.menuFrame=menuFrame;
 		}
 	 
-	 JButton test1;
-	 //GameWindow window;
-		//GameAnimation animation;
 	 MenuFrame menuFrame;
-	
 	
     	@Override
 		public void actionPerformed(ActionEvent e) {
 			 if(LanguageComboBox.getSelectedIndex()==0){
-				 //System.out.println("0");
-				 num=0;
+				
 				LanguageSelectionPolski t= new LanguageSelectionPolski();
 		    	menuFrame.setName(t.menu);
-		    	 
-		    	 
-		    	 
-		 		
+		    
 			 }
 			
 			if(LanguageComboBox.getSelectedIndex()==1){
-				 //System.out.println("1");
-				 
-				LanguageSelectionEnglish t=new LanguageSelectionEnglish();
-				num=1;
 				
+				LanguageSelectionEnglish t=new LanguageSelectionEnglish();
 				menuFrame.setName(t.menu);
-		    	
-		    	
-		 		
+		  
 			 }
 			
-    		
 			}
     	
 			public LanguageChooserListener(){
 				
 	    	 LanguageSelectionEnglish ttt=new LanguageSelectionEnglish();
-	    	 for(int ii=0;ii<=3;ii++){
+	    	 for(int ii=0;ii<=4;ii++){
 		    	  Langmenu[ii]=ttt.menu[ii];
 	    	 }	 
-					    	 
 			
 			 }
  }
 	
+
