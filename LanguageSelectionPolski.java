bb0cca41940c50abca2c5c5b24b8d2d80cac1ba4
maniama <hanabipirate@gmail.com>
2016-05-28 12:48:41 +0200

    Update LanguageSelectionPolski.java

--- LanguageSelectionPolski.java
+++ LanguageSelectionPolski.java
@@ -3,17 +3,28 @@ package pl.edu.pw.fizyka.pojava.OddzialDelta;
 public class LanguageSelectionPolski {
 
 	
-	String  menu[]=new String[4];
+	String  menu[]=new String[5];
 	String missions[]=new String[2];
+	String game[]=new String[9];
 	
 		public LanguageSelectionPolski(){
 			
 		  menu[0]="Start";
-		 menu[1]="Naciœnij przycisk ¿eby rozpocz¹æ";
-	     menu[2]="Wybierz misjê";
-		menu[3]	= "Wybierz jêzyk";
+		  menu[1]="Naciśnij przycisk żeby rozpocząć";
+	      menu[2]="Wybierz misję";
+		  menu[3]= "Wybierz język";
+		  menu[4]="Pomoc";
 			
-		
+		 game[0]="Silnik";
+		 game[1]="Predkosc statku";
+		 game[2]="Current throttle";
+		 game[3]="Aktualna masa statku";
+		 game[4]="Pozostale paliwo";
+		 
+		 game[5]="Nazwa najblizszej planety";
+		 game[6]="Odleglosc do najblizszej planety";
+		 game[7]="X odleglosc do planety";
+		 game[8]="Y odleglosc do planety";
 		 
 		
 		}
