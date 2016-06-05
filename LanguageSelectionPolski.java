4987a8523c7be29cf903957f60bae6c94c2f8734
maniama <hanabipirate@gmail.com>
2016-06-05 13:45:42 +0200

    Update LanguageSelectionPolski.java

--- LanguageSelectionPolski.java
+++ LanguageSelectionPolski.java
@@ -6,7 +6,8 @@ public class LanguageSelectionPolski {
 	String[]  menu=new String[5];
 	String[] missions=new String[2];
 	String[] game=new String[9];
-	
+	String[] planets=new Stirng[9];
+	String [] help=new [1];
 		public LanguageSelectionPolski(){
 			menu=new String[5];
 			menu[0]="Start";
@@ -25,8 +26,41 @@ public class LanguageSelectionPolski {
 			game[6]="Odleg\u0142o\u015b\u0107 do najbli\u017cszej planety";
 			game[7]="X odleg\u0142o\u015b\u0107 do planety";
 			game[8]="Y odleg\u0142o\u015b\u0107 do planety";
-			missions=new String[2];
-		
+			missions=new String[9];
+			missions[0]="Misja 1";
+			missions[1]="Misja 2";
+			missions[2]="Misja 3";
+			missions[3]="Misja 4";
+			missions[4]="Misja 5";
+			missions[5]="Misja 6";
+			missions[6]="Misja 7";
+			missions[7]="Misja 8";
+			missions[8]="Misja 9";
+		        
+		        help=new String[1];
+		        help[0]="W grze …. Wykonujesz misje kosmiczną. Twoje zadanie polega na wylądowaniu rakietą na planecie , wyznaczonej przez wybraną misję. 
+Rakietą steruję się klawiaturą (strzałki) lub myszką(zmiana kąta ruchu). Rakieta zacznie się poruszać po naciśnięciu przycisku Silnik. Paliwo rakiety jest ograniczone.
+Lista misji:
+1.	Misja 1 – Cel: Słońce
+2.	Misja 2 – Cel: Merkury
+3.	Misja 3 – Cel: Wenus 
+4.	Misja 4 - Cel: Mars 
+5.	Misja 5 – Cel: Jowisz 
+6.	Misja 6 – Cel: Saturn 
+7.	Misja 7 -  Cel: Uran
+8.	Misja 8 – Cel: Neptun 
+Każda misja rozpoczyna się na Ziemi.
+";
+                      planets=new String[9];
+                      planets[0]="S\u0142\u0144ce";
+			planets[1]="Merkury";
+			planets[2]="Wenus";
+			planets[3]="Ziemia";
+			planets[4]="Mars";
+			planets[5]="Jowisz";
+			planets[6]="Saturn";
+			planets[7]="Uran";
+                        planets[8]="Neptun";
 		}
 }
 /*Ą - \u0104; ą - \u0105
