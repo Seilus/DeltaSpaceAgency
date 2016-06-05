5843a4c5dae4e78ba3eb394d1dffc9c8514caf39
maniama <hanabipirate@gmail.com>
2016-06-06 00:44:13 +0200

    Update MissionSelectionClass.java

--- src/pl/edu/pw/fizyka/pojava/OddzialDelta/MissionSelectionClass.java
+++ src/pl/edu/pw/fizyka/pojava/OddzialDelta/MissionSelectionClass.java
@@ -3,28 +3,66 @@ package pl.edu.pw.fizyka.pojava.OddzialDelta;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JComboBox;
+
 
 public class MissionSelectionClass implements ActionListener{
-	 public String [] Missions={"Mission 1","Mission 2"}; 
-	 MenuFrame menuFrame;
-	 JComboBox MissionComboBox=new JComboBox(Missions);
+
+	
+	void setMenuFrame(MenuFrame menuFrame){
+		this.menuFrame=menuFrame;
+	}
+	MenuFrame menuFrame;
+		
+	
+	
 @Override
 public void actionPerformed(ActionEvent e) {
-	 if(MissionComboBox.getSelectedIndex()==0){
+	 if(menuFrame.MissionComboBox.getSelectedIndex()==0){
 		 menuFrame.setMission(0);
 		
 	 }
 	
-	if(MissionComboBox.getSelectedIndex()==1){
+	if(menuFrame.MissionComboBox.getSelectedIndex()==1){
 		 menuFrame.setMission(1);
 		
 	 }
+	 if(menuFrame.MissionComboBox.getSelectedIndex()==2){
+		 menuFrame.setMission(2);
+		
+	 }
 	
+	if(menuFrame.MissionComboBox.getSelectedIndex()==3){
+		 menuFrame.setMission(3);
+		
+	 }
+	 if(menuFrame.MissionComboBox.getSelectedIndex()==4){
+		 menuFrame.setMission(4);
+		
+	 }
 	
+	if(menuFrame.MissionComboBox.getSelectedIndex()==5){
+		 menuFrame.setMission(5);
+		
+	 }
+	if(menuFrame.MissionComboBox.getSelectedIndex()==6){
+		 menuFrame.setMission(6);
+		
+	 }
+	
+	if(menuFrame.MissionComboBox.getSelectedIndex()==7){
+		 menuFrame.setMission(7);
+		
+	 }
+	 if(menuFrame.MissionComboBox.getSelectedIndex()==8){
+		 menuFrame.setMission(8);
+		
+	 }
+	
+	if(menuFrame.MissionComboBox.getSelectedIndex()==9){
+		 menuFrame.setMission(9);
 	}
 
-	
+}
 	
 	
 	
