88d7a85181f91df39644daaa4e6697cfc2d83050
maniama <hanabipirate@gmail.com>
2016-06-06 00:41:01 +0200

    Update HelpListener.java

--- src/pl/edu/pw/fizyka/pojava/OddzialDelta/HelpListener.java
+++ src/pl/edu/pw/fizyka/pojava/OddzialDelta/HelpListener.java
@@ -4,17 +4,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class HelpListener implements ActionListener{
-	
-	
+	LanguageSelectionEnglish english;
 	
 	void setMenuFrame(MenuFrame menuFrame){
 		this.menuFrame=menuFrame;
 	}
 	MenuFrame menuFrame;
-	
+	public HelpListener(LanguageSelectionEnglish english){
+		this.english=english;
+		
+		}
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		menuFrame.HelpText.setText("Info");
+		menuFrame.HelpText.setText(english.help[0]);
 		menuFrame.GameHelpOtherPanel.add(menuFrame.HelpText);
 		menuFrame.GameHelpFrame.setSize(400,200);
 		menuFrame.GameHelpFrame.add(menuFrame.GameHelpOtherPanel);
