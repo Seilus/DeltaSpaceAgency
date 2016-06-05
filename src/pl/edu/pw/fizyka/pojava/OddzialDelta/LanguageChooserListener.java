8367d92f21499325a54e2758cb4c58db23555b62
maniama <hanabipirate@gmail.com>
2016-06-06 01:12:32 +0200

    Update LanguageChooserListener.java

--- src/pl/edu/pw/fizyka/pojava/OddzialDelta/LanguageChooserListener.java
+++ src/pl/edu/pw/fizyka/pojava/OddzialDelta/LanguageChooserListener.java
@@ -4,7 +4,7 @@ package pl.edu.pw.fizyka.pojava.OddzialDelta;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
+
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 
@@ -14,31 +14,33 @@ import javax.swing.JLabel;
 	 boolean polish;
 	 public String Langmenu []=new String[5];
 	 public String[] Languages ={"english","polski"};
-	 JComboBox LanguageComboBox=new JComboBox(Languages);
+	 JComboBox<Object> LanguageComboBox=new JComboBox<Object>(Languages);
 	 JLabel test,test2;
 	 MenuFrame menuFrame;
 	 LanguageSelectionEnglish englishLang;
 	 LanguageSelectionPolski polishLang;
-	 
-	 
+	 HelpListener help;
+	 String[] missionsSelection;
 	 void setMenuFrame(MenuFrame menuFrame){
 			this.menuFrame=menuFrame;
 		}
-	    
+	 
 	
     	@Override
 		public void actionPerformed(ActionEvent e) {
 			 if(LanguageComboBox.getSelectedIndex()==0){
-				
+			   menuFrame.setHelpInfo(englishLang.help);
 		    	menuFrame.setName(englishLang.menu);
+		    	
 		    	polish=false;
 		    	// planetInfo.setNamePlanet(t.game);
 		    	//shipStatus.setNameShip(t.game);
 			 }
 			
 			if(LanguageComboBox.getSelectedIndex()==1){
-				
+				menuFrame.setHelpInfo(polishLang.help);
 				menuFrame.setName(polishLang.menu);
+				
 				polish=true;
 		        //planetInfo.setNamePlanet(t.game);
 		        //shipStatus.setNameShip(t.game);
@@ -49,10 +51,12 @@ import javax.swing.JLabel;
 			public LanguageChooserListener(LanguageSelectionEnglish englishLang, LanguageSelectionPolski polishLang){
 				this.englishLang=englishLang;
 				this.polishLang=polishLang;
+				
 				for(int ii=0;ii<=4;ii++){
 					Langmenu[ii]=englishLang.menu[ii];
 				}	 			
 			 }
  }
 	
+
 
