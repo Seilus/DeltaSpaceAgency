6d5220e1c3dcd06b0e561fdf0dc73c039e4b257a
maniama <hanabipirate@gmail.com>
2016-06-20 00:39:40 +0200

    Update GameStartListener.java

--- src/pl/edu/pw/fizyka/pojava/OddzialDelta/GameStartListener.java
+++ src/pl/edu/pw/fizyka/pojava/OddzialDelta/GameStartListener.java
@@ -1,10 +1,15 @@
 package pl.edu.pw.fizyka.pojava.OddzialDelta;
 
+import java.awt.Color;
 import java.awt.KeyboardFocusManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-LanguageChooserListener languageListener;
+import javax.swing.JFrame;
+
+public class GameStartListener implements ActionListener {
+	//Listener that calls constructors for all classes needed for the actual game
+	LanguageChooserListener languageListener;
 	LanguageSelectionEnglish englishLang;
 	LanguageSelectionPolski polishLang;
 	MenuFrame menuFrame;
@@ -18,21 +23,28 @@ LanguageChooserListener languageListener;
 	
 		
 	}
-	
 	void setMenuFrame(MenuFrame menuFrame){
 		this.menuFrame=menuFrame;
 	}
-	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		CelestialBody[] planetSystem=new CelestialBody[3];{
-			planetSystem[0]=new CelestialBody(100000, 0, 0.1, 300, -100, 25);
-			planetSystem[1]=new CelestialBody(2500000, 0, 0, 000, -100, 65);
-			planetSystem[2]=new CelestialBody(100000, 0.025, 0, -100, -300, 20);
+		CelestialBody[] planetSystem=new CelestialBody[9];{
+			//masy
+			planetSystem[0]=new CelestialBody(10000000, 0, 0, 000, -100, 65,Color.YELLOW);
+			planetSystem[1]=new CelestialBody(1.5, -0.0025, 0.1, 80, -100, 4,Color.RED);
+			planetSystem[2]=new CelestialBody(24.7, -0.0025, 0.1, 95, -100, 6,Color.ORANGE);
+			planetSystem[3]=new CelestialBody(30.3, -0.0025, 0.1, 110, -100, 7,Color.BLUE);
+			planetSystem[4]=new CelestialBody(3.25,-0.0025, 0.1, 150 , -100, 5,Color.RED);
+			planetSystem[5]=new CelestialBody(9636.1, -0.0025, 0.1, 205, -100, 25,Color.ORANGE);
+			planetSystem[6]=new CelestialBody(2884.74, -0.0025, 0.1, 345, -100, 20,Color.YELLOW);
+			planetSystem[7]=new CelestialBody(440.57, -0.0025, 0.1, 465 , -100, 10,Color.BLUE);
+			planetSystem[8]=new CelestialBody(517.3, -0.0025, 0.1, 665, -100, 10,Color.BLUE);
+			
+
 		}
 		
 		Ship rocket=new Ship(0, 0);
-		GameMap testMap=new GameMap(planetSystem, rocket);
+		GameMap testMap=new GameMap(planetSystem, rocket,menuFrame);
 		PlanetInfo planetInfo;
 		ShipStatus shipStats;
 		
