d55ea3570ded2f3b84dd180bfc08cc0e24f885c8
maniama <hanabipirate@gmail.com>
2016-06-20 00:44:05 +0200

    Update GravityCalculation.java

--- src/pl/edu/pw/fizyka/pojava/OddzialDelta/GravityCalculation.java
+++ src/pl/edu/pw/fizyka/pojava/OddzialDelta/GravityCalculation.java
@@ -7,7 +7,6 @@ public class GravityCalculation{
 	GravityCalculation(){	
 	}
 	public static void computeGPlanets(CelestialBody[] planetSystem, int frequency){
-		
 		for(int ii=0; ii<planetSystem.length; ii++){
 			double accXkmByS=0;
 			double accYkmByS=0;
@@ -29,16 +28,18 @@ public class GravityCalculation{
 		double accXkmByS=0;
 		double accYkmByS=0;
 		for(int jj=0; jj<planetSystem.length; jj++){
-			double distanceRkm=Math.sqrt(Math.pow(planetSystem[jj].getX(),2)+Math.pow(planetSystem[jj].getY(), 2));
-			if(distanceRkm!=0){
-				
-				accXkmByS+=gravConstantT*planetSystem[jj].getMass()*(planetSystem[jj].getX())/Math.pow(distanceRkm, 3);
-				accYkmByS+=gravConstantT*planetSystem[jj].getMass()*(planetSystem[jj].getY())/Math.pow(distanceRkm, 3);
+			double distanceRkmX=Math.sqrt(Math.pow(planetSystem[jj].getX(),2));
+			double distanceRkmY=Math.sqrt(Math.pow(planetSystem[jj].getY(),2));
+			if(distanceRkmX!=0 && distanceRkmY!=0){
+				//grawitacja do statku nie dziala
+				//accXkmByS+=gravConstantT*planetSystem[jj].getMass()/Math.pow(distanceRkmX, 2);
+				//accYkmByS+=gravConstantT*planetSystem[jj].getMass()/Math.pow(distanceRkmY, 2);
 				
 			}
 		}
 		rocket.accXkmBySGrav=accXkmByS;
 		rocket.accYkmBySGrav=accYkmByS;
+	}
 	public double getDistance(CelestialBody[] planetSystem,int ii){
 		double distanceRkm=Math.sqrt(Math.pow(0-planetSystem[ii].getX(),2)+Math.pow(0-planetSystem[ii].getY(), 2));
 		return distanceRkm;
