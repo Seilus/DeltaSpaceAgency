d3138cbe97a46768e4f2996638ebf8401b4ab9d1
maniama <hanabipirate@gmail.com>
2016-06-20 00:40:34 +0200

    Update HelpListener.java

--- src/pl/edu/pw/fizyka/pojava/OddzialDelta/HelpListener.java
+++ src/pl/edu/pw/fizyka/pojava/OddzialDelta/HelpListener.java
@@ -1,5 +1,7 @@
 package pl.edu.pw.fizyka.pojava.OddzialDelta;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -13,7 +15,7 @@ public class HelpListener implements ActionListener{
 	MenuFrame menuFrame;
 	public HelpListener(LanguageSelectionEnglish english,LanguageSelectionPolski polish){
 		this.english=english;
-		
+		this.polish=polish;
 		}
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -21,15 +23,32 @@ public class HelpListener implements ActionListener{
 			
 			menuFrame.HelpText.setText(polish.help[0]);
 			menuFrame.GameHelpFrame.setSize(550,350);
+			menuFrame.HelpText.setOpaque(false);
+			menuFrame.GameHelpOtherPanel.add(menuFrame.HelpText);
+			//menuFrame.GameHelpFrame.setSize(500,300);
+			menuFrame.GameHelpFrame.add(menuFrame.GameHelpOtherPanel);
+			menuFrame.GameHelpFrame.setVisible(true);
+			menuFrame.GameHelpOtherPanel.repaint();
+		     menuFrame.GameHelpOtherPanel.setOpaque(false);
+		     menuFrame.GameHelpOtherPanel.setFont(new Font("Comic Sans", Font.BOLD, 14));
+		     menuFrame.HelpText.setForeground(Color.WHITE);
+
 		}
 		else{
 		menuFrame.HelpText.setText(english.help[0]);
 		menuFrame.GameHelpFrame.setSize(500,300);
-		}
+		menuFrame.HelpText.setOpaque(false);
 		menuFrame.GameHelpOtherPanel.add(menuFrame.HelpText);
 		//menuFrame.GameHelpFrame.setSize(500,300);
 		menuFrame.GameHelpFrame.add(menuFrame.GameHelpOtherPanel);
 		menuFrame.GameHelpFrame.setVisible(true);
+		menuFrame.GameHelpOtherPanel.repaint();
+	     menuFrame.GameHelpOtherPanel.setOpaque(false);
+	     menuFrame.GameHelpOtherPanel.setFont(new Font("Comic Sans", Font.BOLD, 14));
+	     menuFrame.HelpText.setForeground(Color.WHITE);
+
+		}
+		
 	}
 	
 }
