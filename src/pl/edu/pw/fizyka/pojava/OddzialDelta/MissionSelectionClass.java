ef92ee314bdf76bd428d4510a621284b9efd756d
maniama <hanabipirate@gmail.com>
2016-06-19 11:12:35 +0200

    Update MissionSelectionClass.java

--- src/pl/edu/pw/fizyka/pojava/OddzialDelta/MissionSelectionClass.java
+++ src/pl/edu/pw/fizyka/pojava/OddzialDelta/MissionSelectionClass.java
@@ -17,50 +17,49 @@ public class MissionSelectionClass implements ActionListener{
 	
 @Override
 public void actionPerformed(ActionEvent e) {
-	 if(menuFrame.MissionComboBox.getSelectedIndex()==0){
-		 menuFrame.setMission(0);
-		
-	 }
-	
+	 
 	if(menuFrame.MissionComboBox.getSelectedIndex()==1){
 		 menuFrame.setMission(1);
 		
 	 }
-	 if(menuFrame.MissionComboBox.getSelectedIndex()==2){
+	if(menuFrame.MissionComboBox.getSelectedIndex()==1){
 		 menuFrame.setMission(2);
+		
+	 }
+	 if(menuFrame.MissionComboBox.getSelectedIndex()==2){
+		 menuFrame.setMission(3);
+		 
 		
 	 }
 	
 	if(menuFrame.MissionComboBox.getSelectedIndex()==3){
-		 menuFrame.setMission(3);
+		 menuFrame.setMission(4);
 		
 	 }
 	 if(menuFrame.MissionComboBox.getSelectedIndex()==4){
-		 menuFrame.setMission(4);
+		 menuFrame.setMission(5);
 		
 	 }
 	
 	if(menuFrame.MissionComboBox.getSelectedIndex()==5){
-		 menuFrame.setMission(5);
+		 menuFrame.setMission(6);
 		
 	 }
 	if(menuFrame.MissionComboBox.getSelectedIndex()==6){
-		 menuFrame.setMission(6);
+		 menuFrame.setMission(7);
 		
 	 }
 	
 	if(menuFrame.MissionComboBox.getSelectedIndex()==7){
-		 menuFrame.setMission(7);
-		
-	 }
-	 if(menuFrame.MissionComboBox.getSelectedIndex()==8){
 		 menuFrame.setMission(8);
 		
 	 }
-	
-	if(menuFrame.MissionComboBox.getSelectedIndex()==9){
+	 if(menuFrame.MissionComboBox.getSelectedIndex()==8){
 		 menuFrame.setMission(9);
-	}
+		
+	 }
+	
+	
 
 }
 	
