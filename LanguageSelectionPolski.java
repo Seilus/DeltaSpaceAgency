915d34e858521b2c35ab767dad75dac3417ce642
maniama <hanabipirate@gmail.com>
2016-06-22 00:43:22 +0200

    Update LanguageSelectionPolski.java

--- LanguageSelectionPolski.java
+++ LanguageSelectionPolski.java
@@ -6,63 +6,79 @@ package pl.edu.pw.fizyka.pojava.OddzialDelta;
  */
 public class LanguageSelectionPolski {
 
-	String[]  menu;
-	String[] game;
-	String[] planets;
-	String[] landing;
-	String help;
-	//String[] missions={"Misja 1","Misja 2","Misja 3","Misja 4","Misja 5","Misja 6","Misja 7","Misja 8","Misja 9"};
+	
+	String[]  menu=new String[5];
+	
+	String[] game=new String[9];
+	String[] planets=new String[9];
+	String [] help=new String[1];
+	String[] missions;
+		public LanguageSelectionPolski(){
+			menu=new String[5];
+			menu[0]="Start";
+			menu[1]="Naci\u015bnij przycisk \u017ceby rozpocz\u0105\u0107";
+			menu[2]="Wybierz misj\u0119";
+			menu[3]= "Wybierz j\u0119zyk";
+			menu[4]="Pomoc";
+			game=new String[9];
+			game[0]="SILNIK";
+			game[1]="Predko\u015bc statku";
+			game[2]="Aktualna przepustnica";
+			game[3]="Aktualna masa statku";
+			game[4]="Pozosta\u0142e paliwo";
+		 
+			game[5]="Nazwa najbli\u017cszej planety";
+			game[6]="Odleg\u0142o\u015b\u0107 do najbli\u017cszej planety";
+			game[7]="X odleg\u0142o\u015b\u0107 do planety";
+			game[8]="Y odleg\u0142o\u015b\u0107 do planety";
+			
+		        
+		        help=new String[2];
+		        help[0]="W grze DELTA SPACE AGENCY w1ykonujesz misje kosmiczn\u0105. \nTwoje zadanie polega na wyl\u0105dowaniu rakiet\u0105 na planecie , wyznaczonej przez wybran\u0105 misj\u0119.\n"
++"Rakiet\u0105 steruj\u0119 si\u0119 klawiatur\u0105 (strza\u0142ki) lub myszk\u0105(zmiana k\u0105ta ruchu).\n Rakieta zacznie si\u0119 porusza\u0107 po naci\u015bni\u0119ciu przycisku Silnik.\n Paliwo rakiety jest ograniczone.\n"
++"\nLista misji:\n"
++"1.	Misja 1  Cel: Merkury\n"
++"2.	Misja 2  Cel: Wenus\n"
++"3.	Misja 3  Cel: Ziemia\n"
++"4.	Misja 4  Cel: Mars\n" 
++"5.	Misja 5  Cel: Jowisz\n"
++"6.	Misja 6  Cel: Saturn\n"
++"7.	Misja 7  Cel: Uran\n"
++"8.	Misja 8  Cel: Neptun\n" 
++"Ka\u017cda misja rozpoczyna si\u0119 na Ziemi.\n";
 
-	public LanguageSelectionPolski(){
-		menu=new String[5];
-		menu[0]="Start";
-		menu[1]="Naci\u015bnij przycisk \u017ceby rozpocz\u0105\u0107";
-		menu[2]="Wybierz misj\u0119";
-		menu[3]= "Wybierz j\u0119zyk";
-		menu[4]="Pomoc";
-		game=new String[9];
-		game[0]="SILNIK";
-		game[1]="Predko\u015bc statku";
-		game[2]="Stan przepustnicy";
-		game[3]="Masa statku";
-		game[4]="Pozosta\u0142e paliwo";
-		game[5]="Cel: ";
-		game[6]="Odleg\u0142o\u015b\u0107 do celu: ";
-		game[7]="Prędkość celu: ";
-		game[8]="Odległość do słońca: "; 
-		help=new String();
-		help="Zagubiony w kosmosie.\nTwój statek został uszkodzony i dryfuje w przestrzeni.\nOd ciebie zależy, czy będziesz kontynuował misję, czy też powrócisz na Ziemię.\n"
-		 		+ "Wybierz planetę docelową, i kontynuuj lot\n\nUważaj jednak, gdyż obecny stan rakiety nie pozwoli na\nkontynuowanie lotu po wylądowaniu.\n"
-		 		+ "Monitoruj swoją prędkość, ponieważ system wspomagający lądowanie został wyłączony\nwięc będzie trzeba przeprowadzić lądowanie bardzo delikatnie.\n"
-		 		+ "Jeśli dasz się ponieść rozpaczy, zawsze zostaje samobójczy lot na słońce,\nlub zderzenie z planetą z dużą prędkością.\n\n"
-		 		+ "Niezależnie od wyboru, siadasz za sterami by zaznajomić się z maszyną.\n\n"
-		 		+ "Sterowanie:\n"
-		 		+ "Strzałki- Lewo/Prawo: obrót statku, Góra/Dół: przyspiesz/zwolnij\n"
-		 		+ "Lewy przycisk myszy: obrót statku (z większą precyzją)\n"
-		 		+ "Za pomocą myszy można też sterować silnikiem i wajhą przepustnicy\n"
-		 		+ "Spacja: wyłącz/włącz silnik\n\n"
-		 		+ "Radar w prawym dolnym rogu poprowadzi cię do celu\n"
-		 		+ "Zwracaj uwagę zarówno na niego, jak i na dwa monitory wyświetlające ważne informacje";
-		planets=new String[9];
-        planets[0]="S\u0142o\u0144ce";
-        planets[1]="Merkury";
-		planets[2]="Wenus";
-		planets[3]="Ziemia";
-		planets[4]="Mars";
-		planets[5]="Jowisz";
-		planets[6]="Saturn";
-		planets[7]="Uran";
-        planets[8]="Neptun";
-	}
+            planets=new String[9];
+            planets[0]="";
+			planets[1]="Merkury";
+			planets[2]="Wenus";
+			planets[3]="Ziemia";
+			planets[4]="Mars";
+			planets[5]="Jowisz";
+			planets[6]="Saturn";
+			planets[7]="Uran";
+            planets[8]="Neptun";
+            
+            missions=new String[8];
+           
+            missions[0]= "Misja 1";
+            missions[1]= "Misja 2";
+            missions[2]= "Misja 3";
+            missions[3]= "Misja 4";
+            missions[4]= "Misja 5";
+            missions[5]= "Misja 6";
+            missions[6]= "Misja 7";
+            missions[7]= "Misja 8";
+           
+            
+		}
 }
-/*Ą - \u0104; ą - \u0105
-Ć - \u0106; ć - \u0107
-Ę - \u0118; ę - \u0119
-Ł - \u0141; ł - \u0142
-Ń - \u0143; ń - \u0144
-Ó - \u00d3; ó - \u00f3
-Ś - \u015a; ś - \u015b
-Ź - \u0179; ź - \u017a
-Ż - \u017b; ż - \u017c
+/*Ä„ - \u0104; Ä… - \u0105
+Ä† - \u0106; Ä‡ - \u0107
+Ä� - \u0118; Ä™ - \u0119
+Ĺ� - \u0141; Ĺ‚ - \u0142
+Ĺ� - \u0143; Ĺ„ - \u0144
+Ă“ - \u00d3; Ăł - \u00f3
+Ĺš - \u015a; Ĺ› - \u015b
+Ĺą - \u0179; Ĺş - \u017a
+Ĺ» - \u017b; ĹĽ - \u017c
 */
-
