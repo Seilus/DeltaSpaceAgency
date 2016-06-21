9b234b5781054265db981aa8f90b0568055a6d91
maniama <hanabipirate@gmail.com>
2016-06-22 00:46:03 +0200

    Update LanguageChooserListener.java

--- LanguageChooserListener.java
+++ LanguageChooserListener.java
@@ -3,6 +3,7 @@ package pl.edu.pw.fizyka.pojava.OddzialDelta;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Vector
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 
@@ -14,51 +15,47 @@ import javax.swing.JLabel;
 
  public  class LanguageChooserListener implements ActionListener{
 	 boolean polish;
+	 Vector<String> missions;
 	 public String Langmenu []=new String[5];
 	 public String[] Languages ={"english","polski"};
-
 	 JComboBox<Object> LanguageComboBox=new JComboBox<Object>(Languages);
-
 	 JLabel test,test2;
 	 MenuFrame menuFrame;
 	 LanguageSelectionEnglish englishLang;
 	 LanguageSelectionPolski polishLang;
-	 
 	 HelpListener help;
-	 String[] planets;
+	 String[] missionsSelection;
 	 void setMenuFrame(MenuFrame menuFrame){
 			this.menuFrame=menuFrame;
-			menuFrame.setHelpInfo(englishLang.help);
-	}
+		}
 	 
 	
     	@Override
 		public void actionPerformed(ActionEvent e) {
 			 if(LanguageComboBox.getSelectedIndex()==0){
-				
+			   menuFrame.setHelpInfo(englishLang.help);
 		    	menuFrame.setName(englishLang.menu);
-		    	menuFrame.setHelpInfo(englishLang.help);
-		    	menuFrame.setName(englishLang.menu);
-		    	menuFrame.HelpText.setText(englishLang.help);
-				menuFrame.GameHelpFrame.setSize(500,300);
+		    	for(int ii=0;ii<8;ii++){
+		    		missions.removeElement(polishLang.missions[ii]);
+		    		missions.addElement(englishLang.missions[ii]);
+		            
+		            }
+		    	
 		    	polish=false;
-		    	planets=englishLang.planets;
-		    	menuFrame.setMissionBox();
-		    	menuFrame.validate();
 		    	// planetInfo.setNamePlanet(t.game);
 		    	//shipStatus.setNameShip(t.game);
 			 }
 			
 			if(LanguageComboBox.getSelectedIndex()==1){
-				menuFrame.setName(polishLang.menu);
 				menuFrame.setHelpInfo(polishLang.help);
 				menuFrame.setName(polishLang.menu);
-				menuFrame.HelpText.setText(polishLang.help);
-				menuFrame.GameHelpFrame.setSize(550,350);
-				planets=polishLang.planets;
-				menuFrame.setMissionBox();
+				for(int ii=0;ii<8;ii++){
+					missions.removeElement(englishLang.missions[ii]);
+					missions.addElement(polishLang.missions[ii]);
+	            }
+				
+				
 				polish=true;
-				menuFrame.validate();
 		        //planetInfo.setNamePlanet(t.game);
 		        //shipStatus.setNameShip(t.game);
 			 }
@@ -68,12 +65,18 @@ import javax.swing.JLabel;
 			public LanguageChooserListener(LanguageSelectionEnglish englishLang, LanguageSelectionPolski polishLang){
 				this.englishLang=englishLang;
 				this.polishLang=polishLang;
-				//menuFrame.HelpText.setText(englishLang.help);
-				//menuFrame.GameHelpFrame.setSize(500,300);
+				missions=new  Vector<String>(); 
 				for(int ii=0;ii<=4;ii++){
 					Langmenu[ii]=englishLang.menu[ii];
-				}
-				planets=new String[9];
-				planets=englishLang.planets;
+				}	 			
+				
+
+					for(int ii=0;ii<8;ii++){
+						missions.addElement(englishLang.missions[ii]);
+			            
+			            }
+				
+
 			 }
- }	
+ }
+	
